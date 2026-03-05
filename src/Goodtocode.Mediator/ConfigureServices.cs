53f7cc006da31b9969379c25ce05f8a3941e10a1
Robert Good <robert.good@goodtocode.com>
2026-03-04 22:19:23 -0800

    Handle for executing or calling or custom

--- src/Goodtocode.Mediator/ConfigureServices.cs
+++ src/Goodtocode.Mediator/ConfigureServices.cs
@@ -7,28 +7,41 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddMediatorServices(this IServiceCollection services)
     {
-        var assembly = Assembly.GetExecutingAssembly();
+        var executingAssembly = Assembly.GetExecutingAssembly();
+        var callingAssembly = Assembly.GetCallingAssembly();
 
-        var handlerTypes = assembly
-            .GetTypes()
-            .Where(t => t.GetInterfaces().Any(i =>
-                i.IsGenericType &&
-                (
-                    i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>) ||
-                    i.GetGenericTypeDefinition() == typeof(IRequestHandler<>)
-                )
-            ));
+        return services.AddMediatorServices(executingAssembly, callingAssembly);
+    }
 
-        foreach (var handlerType in handlerTypes)
+    public static IServiceCollection AddMediatorServices(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        if (assemblies == null || assemblies.Length == 0)
+            return services;
+        var assembliesToScan = new HashSet<Assembly>(assemblies);
+
+        foreach (var assembly in assembliesToScan)
         {
-            var interfaceType = handlerType.GetInterfaces().First(i =>
-                i.IsGenericType &&
-                (
-                    i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>) ||
-                    i.GetGenericTypeDefinition() == typeof(IRequestHandler<>)
-                )
-            );
-            services.AddTransient(interfaceType, handlerType);
+            var handlerTypes = assembly
+             .GetTypes()
+             .Where(t => t.GetInterfaces().Any(i =>
+                 i.IsGenericType &&
+                 (
+                     i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>) ||
+                     i.GetGenericTypeDefinition() == typeof(IRequestHandler<>)
+                 )
+             ));
+
+            foreach (var handlerType in handlerTypes)
+            {
+                var interfaceType = handlerType.GetInterfaces().First(i =>
+                    i.IsGenericType &&
+                    (
+                        i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>) ||
+                        i.GetGenericTypeDefinition() == typeof(IRequestHandler<>)
+                    )
+                );
+                services.AddTransient(interfaceType, handlerType);
+            }
         }
 
         services.AddTransient<IRequestDispatcher, RequestDispatcher>();
@@ -37,5 +50,3 @@ public static class ConfigureServices
         return services;
     }
 }
-
-
